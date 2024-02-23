f2a4e6f3517fae9586a81ae321859aa61cc91a74
olya <fgfg>
2024-02-23 11:25:36 +0300

    add lesson2:task3

---
+++ Lesson2/task3/Program.cs
@@ -0,0 +1,10 @@
+﻿//int n = 10;
+int[] arr = { 2, 3, 4, 5, 8, 35, 1, 3, 8, 5 };
+int i = 0;
+
+while(i < arr.Length) {
+    if(arr[i] % 2 == 0) {
+        System.Console.Write($"{arr[i]} ");
+    }
+    i++;
+}
