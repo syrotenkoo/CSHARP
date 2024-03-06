c9ab792688ba676979a1cdbdf96bb29109a77014
olya <fgfg>
2024-03-07 02:08:05 +0300

    add lesson3:task1

---
+++ Lesson3/task1/Program.cs
@@ -0,0 +1,10 @@
+﻿double CalculateFormule(int a, int b, int c, int d)
+{
+    double num = a * b;
+    int denum = c + d;
+    double res = num / denum; 
+    return res;
+}
+
+double result = CalculateFormule(1, 2, 3, 4);
+Console.WriteLine(result);
